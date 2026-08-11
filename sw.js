efe391c8a478c5bd39e8f0edd0391df8e1bb563b
Lord Cebal <19960107+LordCebal@users.noreply.github.com>
2026-08-11 20:25:41 +0000

    Initial setup

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './style.css'
+];
+
+// Install Event
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
